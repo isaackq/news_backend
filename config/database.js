c36c2636e8fde6ce1df6bc9d0371d04e4929c344
isaac kamel <isaackamel12345@gmail.com>
2025-01-18 13:53:23 +0200

    edit the database settengs and add env file

--- config/database.js
+++ config/database.js
@@ -1,14 +1,30 @@
 // import sequalize from "sequalize";
 // const sequalize = require("sequalize");
-const { Sequelize } = require("sequelize");
+// const { Sequelize } = require("sequelize");
 
-const sequalize = new Sequelize({
-  host: "localhost",
-  port : 3308,//عشان التعارض مع الوامب 
-  username: "root",
-  password: "",
-  database : "distributed",
-  dialect : "mysql"//نوع الداتا بيز الي بدي اتصل عليها 
+// const sequalize = new Sequelize({
+//   host: "localhost",
+//   port : 3308,//عشان التعارض مع الوامب 
+//   username: "root",
+//   password: "",
+//   database : "distributed",
+//   dialect : "mysql"//نوع الداتا بيز الي بدي اتصل عليها 
+// });
+
+// module.exports = sequalize;
+
+
+const { Sequelize } = require("sequelize");
+require("dotenv").config(); // To load environment variables from the .env file
+
+const sequelize = new Sequelize({
+  host: process.env.DB_HOST,     // Database host
+  port: process.env.DB_PORT,     // Database port
+  username: process.env.DB_USER, // Database username
+  password: process.env.DB_PASS, // Database password
+  database: process.env.DB_NAME, // Database name
+  dialect: process.env.DB_DIALECT, // Database dialect
 });
 
-module.exports = sequalize;
+module.exports = sequelize;
+
